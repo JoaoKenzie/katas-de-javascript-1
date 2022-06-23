97da7d2220c5571b1a4f84814da3e33417812618
João Paulo Oliveira Barreto <106703587+JoaoKenzie@users.noreply.github.com>
2022-06-23 19:52:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+//Questão 1  
+for (let index = 1; index <= 20; index++) {
+    console.log(index)
+}
+
+//Questão 2
+for (let index = 1; index <= 20; index++){
+    if(index %2 == 0){
+        console.log(index)
+    }
+}
+
+//Questão 3
+for (let index = 1; index <= 20; index++){
+    if(index %2){
+        console.log(index)
+    }
+}
+
+//Questão 4
+for (let index = 5; index <= 100; index++){
+    if(index %5 == 0){
+        console.log(index)
+    }
+}
+
+//Questão 5
+for (let index = 1; index <= 100; index++){
+    if(Math.sqrt(index) % 1 === 0){
+        console.log(index)
+    }
+}
+
+//Questão 6
+for (let index = 20; index >= 1; index--) {
+    console.log(index)
+}
+
+//Questão 7
+for (let index = 20; index >= 1; index--) {
+    if(index %2 == 0)
+        console.log(index)
+}
+
+//Questão 8
+for (let index = 20; index >= 1; index--) {
+    if(index %2)
+        console.log(index)
+}
+
+//Questão 9
+for (let index = 100; index >= 5; index--) {
+    if(index %5 == 0)
+        console.log(index)
+}
+
+//Questão 10
+for (let index = 100; index >= 1; index--) {
+    if(Math.sqrt(index) % 1 === 0){
+        console.log(index)
+    }
+}
